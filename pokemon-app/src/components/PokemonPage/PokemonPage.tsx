789ecaca8842112fde806393725e0e99fd3b001e
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2021-04-06 18:16:33 +0300

    feat: create about page first design

--- pokemon-app/src/components/PokemonPage/PokemonPage.tsx
+++ pokemon-app/src/components/PokemonPage/PokemonPage.tsx
@@ -13,6 +13,61 @@ const PokemonPage: React.FC = () => {
 
     return (
         <div className='about'>
+            <div className='pokemon-header f-c'><span>Pokemon #{id}</span></div>
+            <div className='abilities f-c'>
+                <div className='type'><span>Type1</span></div>
+                <div className='type'><span>Type1</span></div>
+            </div>
+            <div className='pokemon-image'>
+                <div className='img-wrap f-c'>
+                    <img src="https://pokeres.bastionbot.org/images/pokemon/2.png" alt="2" />
+                </div>
+            </div>
+            <div className='abilities f-c'>
+                <div className='ability'><span>ability1</span></div>
+                <div className='ability'><span>ability2</span></div>
+                <div className='ability'><span>ability3</span></div>
+            </div>
+
+            <div className='info f-c'>
+                <div className='info-block'>
+                    <div className='info-header f-c'><span>Stats</span></div>
+                    <div className='info-text'><span>Stat name 1</span></div>
+                    <div className='stat-wrap'>
+                        <div className='stat-field-1 stat-field'></div>
+                        <span>50</span>
+                    </div>
+                    <div className='info-text'><span>Stat name 2</span></div>
+                    <div className='stat-wrap'>
+                        <div className='stat-field-2 stat-field'></div>
+                        <span>70</span>
+                    </div>
+                    <div className='info-text'><span>Stat name 3</span></div>
+                    <div className='stat-wrap'>
+                        <div className='stat-field-3 stat-field'></div>
+                        <span>30</span>
+                    </div>
+                </div>
+                <div className='info-block'>
+                    <div className='info-header f-c'><span>Parametres</span></div>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Weigth</td>
+                                <td>100</td>
+                            </tr>
+                            <tr>
+                                <td>Height</td>
+                                <td>100</td>
+                            </tr>
+                            <tr>
+                                <td>Base expirience</td>
+                                <td>100</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
             <h1>This is pokemon page #{id}</h1>
             <Link to={randomPath}><h3>Go to random pokemon</h3></Link>
         </div>
