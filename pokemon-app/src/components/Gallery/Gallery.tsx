fe0004309ce219e3da028645f1f74edfa72575f9
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2021-04-06 10:12:17 +0300

    fix: resolve console warnings

--- pokemon-app/src/components/Gallery/Gallery.tsx
+++ pokemon-app/src/components/Gallery/Gallery.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { RootStateOrAny, useDispatch, useSelector } from 'react-redux'
+import React from 'react'
+import { RootStateOrAny, useSelector } from 'react-redux'
 import { Pokemons } from './Pokemons'
 import { Pokemon } from '../../types'
 
--- pokemon-app/src/components/Gallery/Pokemons.tsx
+++ pokemon-app/src/components/Gallery/Pokemons.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { RootStateOrAny, useSelector } from 'react-redux'
 import { Pokemon } from '../../types'
 import { PokemonCard } from './PokemonCard'
